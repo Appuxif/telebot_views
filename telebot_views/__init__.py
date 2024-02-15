e1064fba7efde17884d46780454973e0439f19c1
Smokost <smokost@gmail.com>
2024-02-15 21:18:30 +0100

    Handle exceptions

--- telebot_views/__init__.py
+++ telebot_views/__init__.py
@@ -1,3 +1,5 @@
+from logging import getLogger
+
 from telebot.async_telebot import AsyncTeleBot
 from telebot.types import CallbackQuery, Message
 
@@ -5,6 +7,8 @@ from telebot_views import bot
 from telebot_views.base import Request, Route, RouteResolver
 from telebot_views.dispatcher import ViewDispatcher
 from telebot_views.dummy import DummyView
+
+logger = getLogger('telebot_views')
 
 
 def set_bot(tele_bot: AsyncTeleBot):
@@ -19,18 +23,26 @@ def init(tele_bot: AsyncTeleBot, routes: list[Route], skip_non_private: bool = F
 
     @tele_bot.message_handler()
     async def message_handler(msg: Message):
-        if skip_non_private and msg.chat.type != 'private':
-            return
-        if msg.from_user.id == tele_bot.token.split(':', 1)[0]:
-            return
+        try:
+            if skip_non_private and msg.chat.type != 'private':
+                return
+            if msg.from_user.id == tele_bot.token.split(':', 1)[0]:
+                return
 
-        request = Request(msg=msg)
-        await ViewDispatcher(request=request).dispatch()
+            request = Request(msg=msg)
+            await ViewDispatcher(request=request).dispatch()
+        except Exception:
+            logger.exception('message_handler error')
+            raise
 
     @tele_bot.callback_query_handler(func=lambda call: True)
     async def callback_query(callback: CallbackQuery):
-        if skip_non_private and callback.message.chat.type != 'private':
-            return
+        try:
+            if skip_non_private and callback.message.chat.type != 'private':
+                return
 
-        request = Request(callback=callback)
-        await ViewDispatcher(request=request).dispatch()
+            request = Request(callback=callback)
+            await ViewDispatcher(request=request).dispatch()
+        except Exception:
+            logger.exception('callback_query error')
+            raise
