8e4e0dbdc22ac894aae0642b771e59776e56caf8
Smokost <smokost@gmail.com>
2024-03-25 21:08:46 +0100

    Add with_cache decorator + promote to 0.0.12

--- telebot_views/models/cache.py
+++ telebot_views/models/cache.py
@@ -1,7 +1,7 @@
-from datetime import datetime
-from typing import ClassVar, Type
+from datetime import datetime, timedelta
+from typing import Any, Callable, ClassVar, Type
 
-from pydantic import Field
+from pydantic import BaseModel, Field
 from telebot_models.models import BaseModelManager, Model
 
 from telebot_views.utils import now_utc
@@ -28,3 +28,30 @@ class CacheModelManager(BaseModelManager[CacheModel]):
 
     def is_valid(self) -> 'CacheModelManager':
         return self.filter({'valid_until': {'$gt': now_utc()}})
+
+
+def with_cache(
+    cache_key: str,
+    cache_ttl: int,
+    serializer: Callable[[Any], dict] = lambda x: x,
+    deserializer: Callable[[dict], Any] = lambda x: x,
+):
+    def decorator(func):
+        async def wrapper(*args: Any, **kwargs: Any) -> Any:
+            cache = await CacheModel.manager().by_key(cache_key).is_valid().find_one(raise_exception=False)
+            if cache is not None:
+                return deserializer(cache.data)
+
+            result = await func(*args, **kwargs)
+
+            await CacheModel(
+                key=cache_key,
+                data=serializer(result),
+                valid_until=now_utc() + timedelta(seconds=cache_ttl),
+            ).insert()
+
+            return result
+
+        return wrapper
+
+    return decorator
