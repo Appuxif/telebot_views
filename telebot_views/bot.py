85c864fa13dfc0232f1cf168604db6bb0323901e
Smokost <smokost@gmail.com>
2024-03-27 17:09:07 +0100

    Add parse mode
    + promote to 0.0.15

--- telebot_views/bot.py
+++ telebot_views/bot.py
@@ -1,3 +1,14 @@
+from enum import Enum
+
 from telebot.async_telebot import AsyncTeleBot
 
 bot: AsyncTeleBot = AsyncTeleBot('dummy_token')
+
+
+class ParseMode(str, Enum):
+    """Parse Mode Enum"""
+
+    NONE = ''
+    MARKDOWN = 'Markdown'
+    HTML = 'HTML'
+    MARKDOWN_V2 = 'MarkdownV2'
