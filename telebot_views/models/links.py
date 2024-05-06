c2d4f829849c3932e666b1933b79ce69689bdeb3
Smokost <smokost@gmail.com>
2024-05-06 20:38:30 +0200

    Add links view
    + promote to 0.0.19

---
+++ telebot_views/models/links.py
@@ -0,0 +1,44 @@
+from logging import getLogger
+from typing import ClassVar, Type
+
+from pymongo import ReturnDocument
+from telebot_models.models import BaseModelManager, Model
+
+from telebot_views import bot
+from telebot_views.models.users import UserStateCb
+
+logger = getLogger(__name__)
+
+
+class LinkModel(Model):
+    """Link"""
+
+    callback: UserStateCb
+
+    manager: ClassVar[Type['LinkModelManager']]
+
+    def get_bot_start_link(self) -> str:
+        return f't.me/{bot.bot.user.username}?start=link_{self.id}'
+
+
+class LinkModelManager(BaseModelManager[LinkModel]):
+    """Links Model Manager"""
+
+    collection = 'links'
+    model = LinkModel
+
+    async def get_or_create(self, model: LinkModel) -> LinkModel:
+        exclude = {'id': True, 'callback': {'id': True, 'created_at': True}}
+        obj = model.dict(by_alias=True, exclude=exclude)
+        logger.debug(
+            'Creating link:\nexclude=%s\nobj=%s',
+            exclude,
+            obj,
+        )
+        document = await self.get_collection().find_one_and_update(
+            obj,
+            {'$set': obj},
+            upsert=True,
+            return_document=ReturnDocument.AFTER,
+        )
+        return self.model.parse_obj(document)
