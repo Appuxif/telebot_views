309840f23ac20c697dc4e5bfb23510dfa76d03e4
Konstantin Smoliarov <smokost@gmail.com>
2024-09-16 11:42:12 +0200

    Add business_connection_id

--- telebot_views/services/subscriptions.py
+++ telebot_views/services/subscriptions.py
@@ -1,4 +1,5 @@
 from logging import getLogger
+from typing import Optional
 
 from telebot.asyncio_helper import ApiTelegramException
 from telebot.types import ChatMemberAdministrator, ChatMemberMember, ChatMemberOwner, ChatMemberRestricted
@@ -33,21 +34,23 @@ async def check_subscription(chat_id: int, user_id: int) -> bool:
     return result
 
 
-async def ensure_subscription(chat_id: int, user_id: int, force: bool = False) -> bool:
+async def ensure_subscription(
+    sub_chat_id: int, chat_id: int, user_id: int, force: bool = False, business_connection_id: Optional[str] = None
+) -> bool:
     """Проверяет подписку пользователя на канал в кэше.
     Если валидный кэш отсутствует, то записывает результат в кэш.
 
     Если подписки нет, то отправляет уведомление с необходимостью подписаться на канал.
     """
 
-    cache_key = f'chat:{chat_id}:user:{user_id}:sub'
+    cache_key = f'sub_chat:{sub_chat_id}:user:{user_id}:sub'
 
     @with_cache(cache_key, 60 * 5, force=force)
     async def _inner() -> dict[str, bool]:
-        subscription_result = await check_subscription(chat_id, user_id)
+        subscription_result = await check_subscription(sub_chat_id, user_id)
         result = {'subscription_result': subscription_result}
         if not subscription_result:
-            chat = await get_chat(chat_id)
+            chat = await get_chat(sub_chat_id)
             result['chat_title'] = chat.title
             result['chat_username'] = chat.username
         return result
@@ -58,8 +61,9 @@ async def ensure_subscription(chat_id: int, user_id: int, force: bool = False) -
         chat_title = data['chat_title']
         chat_username = data['chat_username']
         await bot.bot.send_message(
-            user_id,
+            chat_id,
             f'⛔ Чтобы продолжить, нужно подписаться на канал:\n{chat_title}\n@{chat_username}',
+            business_connection_id=business_connection_id,
         )
 
     return data['subscription_result']
